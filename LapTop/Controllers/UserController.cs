21e30bc9e07378b506f9f8edebe3cdfc22beef6f
honghai05127 <honghai051201@gmail.com>
2021-10-20 13:49:35 +0700

    push
    push

---
+++ LapTop/Controllers/UserController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LapTop.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace LapTop.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public UserController(IConfiguration config)
+        {
+            _configuration = config;
+        }
+
+
+        // GET: UserController
+        public ActionResult Index()
+        {
+            string connStr = _configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            List<User> list = new List<User>();
+            string query = "select * from _CODE_TAIKHOAN";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new User()
+                        {
+                            Tentaikhoan = reader["TENDANGNHAP"].ToString(),
+                            Matkhau = reader["MATKHAU"].ToString()
+                        });
+
+                    }
+
+                }
+            }
+            return View(list);
+        }
+
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        public IActionResult Create(User um)
+        {
+            try
+            {
+                string connStr = _configuration.GetConnectionString("DefaultConnection");
+                SqlConnection conn = new SqlConnection(connStr);
+                if (!check_exists(um.Tentaikhoan))
+                {
+                    ModelState.AddModelError("", "Tên tài khoản đã tồn tại");
+                    
+                }
+                conn.Open();
+                string query = "INSERT INTO [dbo].[_CODE_TAIKHOAN]([TENDANGNHAP],[MATKHAU])VALUES(@tendangnhap,@matkhau)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@tendangnhap", um.Tentaikhoan);
+                cmd.Parameters.AddWithValue("@matkhau", um.Matkhau);
+
+                cmd.ExecuteNonQuery();
+
+                conn.Close();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(um);
+            }
+            
+        }
+        [NonAction]
+        public bool check_exists(string tentaikhoan)
+        {
+            string connStr = _configuration.GetConnectionString("DefaultConnection");
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            List<User> list = new List<User>();
+            string query = "select * from _CODE_TAIKHOAN";
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new User()
+                        {
+                            Tentaikhoan = reader["TENDANGNHAP"].ToString(),
+                            Matkhau = reader["MATKHAU"].ToString()
+                        });
+
+                    }
+
+                }
+            }
+            foreach(User temp in list)
+            {
+                if (temp.Tentaikhoan == tentaikhoan) return false;
+            }
+            return true;
+            }
+
+    }
+}
---
+++ LapTop/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LapTop.Models
+{
+    public class User
+    {
+        private string _tentaikhoan;
+        private string _matkhau;
+
+        public User()
+        {
+        }
+
+        public User(string tentaikhoan, string matkhau)
+        {
+            _tentaikhoan = tentaikhoan;
+            _matkhau = matkhau;
+        }
+
+        public string Tentaikhoan { get => _tentaikhoan; set => _tentaikhoan = value; }
+        public string Matkhau { get => _matkhau; set => _matkhau = value; }
+    }
+}
+
